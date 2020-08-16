e2abbc4618bc1fffe5a4a6af979e81f0111aaadb
tanyachandwani <ruchitachandwani@gmail.com>
2020-08-16 17:50:19 +0530

    created arrays

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
@@ -9,5 +11,14 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if (this.body.speed>10 && this.body.position.x>220){
+    var pos=[this.body.position.x,this.body.position.y];
+    this.trajectory.push(pos);
+  }
+    for(var i=0;i<this.trajectory.length;i++){
+       image(this.smokeimage,this.trajectory[i][0],this.trajectory[i][1])
+
+    }
+    
   }
 }
